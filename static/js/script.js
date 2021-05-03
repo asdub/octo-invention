00b426803d418db1e2a6a96795b4324d33ca7738
asdub <adam.smitzer@gmail.com>
2021-05-03 19:02:47 +0000

    Add task collapsible setup'.

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {edge: "right"});
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
   });
