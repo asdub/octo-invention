89062904be5e4687839c02478a01c10e8751383a
asdub <adam.smitzer@gmail.com>
2021-05-03 19:25:20 +0000

    Bind data from mongodb top task acordian

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {edge: "right"});
-    var elems = document.querySelectorAll('.collapsible');
-    var instances = M.Collapsible.init(elems);
+    var task = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(task);
+    var tooltip = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(tooltip, {
+                                    position: "top", 
+                                    html: "Urgent Task!",
+                                    margin: 0.1
+                                  });
   });
