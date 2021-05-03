56a2a857edb16a679aacbd48d965e49ad45b9f62
asdub <adam.smitzer@gmail.com>
2021-05-03 20:23:07 +0000

    Added fields for new task

--- static/js/script.js
+++ static/js/script.js
@@ -5,8 +5,12 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.Collapsible.init(task);
     var tooltip = document.querySelectorAll('.tooltipped');
     var instances = M.Tooltip.init(tooltip, {
-                                    position: "top", 
-                                    html: "Urgent Task!",
-                                    margin: 0.1
-                                  });
+                                              position: "top", 
+                                              html: "Urgent Task!",
+                                              margin: 0.1
+                                            });
+    var datepicker = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(datepicker, {
+                                                  autoClose: "True", 
+                                                  format: "dd mmm, yyyy",});
   });
